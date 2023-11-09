cc31dec35c29be45c37f0c3d76a71be0c074dd72
yeshiworkgeorge <yeshiworkgeorge@gmail.com>
2023-11-09 15:01:23 -0800

    neww

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -4,7 +4,7 @@ const instance = axios.create({
 	// THE API (cloud function) URL
 	// baseURL: "http://127.0.0.1:5001/pp-741c0/us-central1/api",
 
-	baseURL: "https://cobalt-blue-meerkat-boot.cyclic.app",
+	baseURL: "https://fair-lime-gorilla-gown.cyclic.app",
 });
 
 export default instance;
