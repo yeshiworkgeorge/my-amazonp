f8b74b23fc9d299ae88962cadcf60e790098c6ae
yeshiworkgeorge <yeshiworkgeorge@gmail.com>
2023-11-09 15:50:16 -0800

    nbv

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -4,7 +4,7 @@ const instance = axios.create({
 	// THE API (cloud function) URL
 	// baseURL: "http://127.0.0.1:5001/pp-741c0/us-central1/api",
 
-	baseURL: "",
+	baseURL: "https://teal-lovely-coyote.cyclic.app",
 });
 
 export default instance;
